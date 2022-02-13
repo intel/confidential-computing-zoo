5f807e02c87fb953948be078a727af2f53e8df97
Hsy-Intel <siyuan.hui@intel.com>
2022-02-13 21:53:06 +0800

    hfl

--- cczoo/horizontal_fl/hfl-tensorflow/test-sgx.sh
+++ cczoo/horizontal_fl/hfl-tensorflow/test-sgx.sh
@@ -50,8 +50,8 @@ function make_custom_env() {
 ROLE=$1
 PS_HOSTS=$2
 WORKER_HOSTS=$3
+
 if [ "$ROLE" == "make" ]; then
-    # rm -rf model *.log
     make clean && make | make_logfilter
 elif [ "$ROLE" == "ps0" ]; then
     make_custom_env
@@ -59,13 +59,6 @@ elif [ "$ROLE" == "ps0" ]; then
     if [ "$DEBUG" != "0" ]; then
         wait && kill -9 `pgrep -f gramine`
     fi
-elif [ "$ROLE" == "ps1" ]; then
-    make_custom_env
-    taskset -c 4-7 stdbuf -o0 gramine-sgx python -u train.py --task_index=1 --job_name=ps $PS_HOSTS $WORKER_HOSTS 2>&1  | runtime_logfilter | tee -a ps1-gramine-python.log &
-    if [ "$DEBUG" != "0" ]; then
-        wait && kill -9 `pgrep -f gramine`
-    fi
-
 elif [ "$ROLE" == "worker0" ]; then
     make_custom_env
     taskset -c 8-11 stdbuf -o0 gramine-sgx python -u train.py --task_index=0 --job_name=worker $PS_HOSTS $WORKER_HOSTS 2>&1 | runtime_logfilter | tee -a worker0-gramine-python.log &
