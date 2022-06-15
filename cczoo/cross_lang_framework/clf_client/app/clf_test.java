9613de05c591dd6cf12d75bab1885e2a123858df
hhr293 <17888821684@163.com>
2022-06-15 10:40:54 +0800

    Update clf_test.java

--- cczoo/cross_lang_framework/clf_client/app/clf_test.java
+++ cczoo/cross_lang_framework/clf_client/app/clf_test.java
@@ -13,7 +13,7 @@ public class clf_test {
 
 	public static void jni_test()
 	{
-		String ip_port = "VM-0-3-ubuntu:4433";
+		String ip_port = "localhost:4433";
 		String ca_cert = "certs/ca_cert.crt";
 		gramine_jni jni_so = new gramine_jni(ip_port.getBytes(), ca_cert.getBytes());
 
