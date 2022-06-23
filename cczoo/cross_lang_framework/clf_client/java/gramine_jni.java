dbdd63ac8393ee800db0b9f6290d901cc0b7977a
Liang Fang <liang.a.fang@intel.com>
2022-06-23 17:27:43 +0800

    Fix JNI String bug
    Signed-off-by: Liang Fang <liang.a.fang@intel.com>

--- cczoo/cross_lang_framework/clf_client/java/gramine_jni.java
+++ cczoo/cross_lang_framework/clf_client/java/gramine_jni.java
@@ -1,19 +1,19 @@
 package GramineJni;
 
 public class gramine_jni {
-	public native int get_key(byte[] ip_port, byte[] ca_cert, byte[] key, int key_len);
-	public native int get_file_2_buff(byte[] ip_port, byte[] ca_cert, byte[] fname, long offset, byte[] data, int len, int[] ret_len);
-	public native int get_file_size(byte[] ip_port, byte[] ca_cert, byte[] fname, long[] ret_len);
-	public native int put_result(byte[] ip_port, byte[] ca_cert, byte[] fname, long offset, byte[] data, int len, int[] ret_len);
+	public native int get_key(String ip_port, String ca_cert, byte[] key, int key_len);
+	public native int get_file_2_buff(String ip_port, String ca_cert, String fname, long offset, byte[] data, int len, int[] ret_len);
+	public native int get_file_size(String ip_port, String ca_cert, String fname, long[] ret_len);
+	public native int put_result(String ip_port, String ca_cert, String fname, long offset, byte[] data, int len, int[] ret_len);
 
 	static {
 		System.loadLibrary("gramine_jni");
 	}
 
-	byte[] m_ip_port;
-	byte[] m_ca_cert;
+	String m_ip_port;
+	String m_ca_cert;
 
-	public gramine_jni(byte[] ip_port, byte[] ca_cert) {
+	public gramine_jni(String ip_port, String ca_cert) {
 		m_ip_port = ip_port;
 		m_ca_cert = ca_cert;
 	}
@@ -28,7 +28,7 @@ public class gramine_jni {
 		return ret;
 	}
 
-	public int GetFile2Buff(byte[] fname, long offset, byte[] data, int len, int[] ret_len) throws Exception {
+	public int GetFile2Buff(String fname, long offset, byte[] data, int len, int[] ret_len) throws Exception {
 		int ret = 0;
 		try {
 			ret = get_file_2_buff(m_ip_port, m_ca_cert, fname, offset, data, len, ret_len);
@@ -38,7 +38,7 @@ public class gramine_jni {
 		return ret;
 	}
 
-	public int GetFileSize(byte[] fname, long[] ret_len) throws Exception {
+	public int GetFileSize(String fname, long[] ret_len) throws Exception {
 		int ret = 0;
 		try {
 			ret = get_file_size(m_ip_port, m_ca_cert, fname, ret_len);
@@ -48,7 +48,7 @@ public class gramine_jni {
 		return ret;
 	}
 
-	public int PutResult(byte[] fname, long offset, byte[] data, int len, int[] ret_len) throws Exception {
+	public int PutResult(String fname, long offset, byte[] data, int len, int[] ret_len) throws Exception {
 		int ret = 0;
 		try {
 			ret = put_result(m_ip_port, m_ca_cert, fname, offset, data, len, ret_len);
