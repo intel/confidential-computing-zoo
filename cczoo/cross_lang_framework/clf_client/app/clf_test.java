f659703329d8a8e6ad28d1bfa2fee25e53e77fb8
Liang Fang <liang.a.fang@intel.com>
2022-06-23 17:27:43 +0800

    Fix JNI String bug
    Signed-off-by: Liang Fang <liang.a.fang@intel.com>

--- cczoo/cross_lang_framework/clf_client/app/clf_test.java
+++ cczoo/cross_lang_framework/clf_client/app/clf_test.java
@@ -8,23 +8,25 @@ import GramineJni.*;
 
 public class clf_test {
 	public static void main(String[] args) throws InterruptedException {
-		//jni_local_test();
-		jni_remote_test();
+		for(int i = 0; i<1; i++) {
+			//jni_local_test();
+			jni_remote_test();
+		}
 	}
 
 	public static void jni_local_test()
 	{
 		String ip_port = "local";
 		String ca_cert = "";
-		gramine_jni jni_so = new gramine_jni(ip_port.getBytes(), ca_cert.getBytes());
+		gramine_jni jni_so = new gramine_jni(ip_port, ca_cert);
 
 		// demonstrate get a server file size
-		//String fname = "/plain/plain.txt";
-		String fname = "/readonly/1.txt";
+		String fname = "/plain/plain.txt";
+		//String fname = "/readonly/1.txt";
 		System.out.format("\n[test] get file size\n");
 		long[] data_len = new long[1];
 		try {
-			jni_so.GetFileSize(fname.getBytes(), data_len);
+			jni_so.GetFileSize(fname, data_len);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
@@ -34,22 +36,22 @@ public class clf_test {
 		byte[] data = new byte[(int)data_len[0]];
 		long offset = 0;
 		int len = 32;
-		for(int i = 0; i<10; i++) {
+		for(int i = 0; i<1000; i++) {
 			System.out.format("\n[test] get data from local plain filesystem. %s, offset=%d, expect len=%d\n", fname, offset, len);
 			try {
 				int[] ret_len = new int[1];
-				jni_so.GetFile2Buff(fname.getBytes(), offset, data, len, ret_len);
+				jni_so.GetFile2Buff(fname, offset, data, len, ret_len);
 				print_array(data, ret_len[0]);
-				offset += len;
-				if(offset > (int)data_len[0])
-					offset = 0;
-/*
+
 				int[] ret_len2 = new int[1];
 				String output_fname = "/plain/result.out";
 				System.out.format("\n[test] write local plain file, %s\n", output_fname);
-				jni_so.PutResult(output_fname.getBytes(), offset, data, ret_len[0], ret_len2);
+				jni_so.PutResult(output_fname, offset, data, ret_len[0], ret_len2);
 				System.out.format("jni_so.PutResult(%s)\n", output_fname);
-*/
+
+				offset += len;
+				if(offset > (int)data_len[0])
+					offset = 0;
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 				break;
@@ -62,7 +64,7 @@ public class clf_test {
 	{
 		String ip_port = "localhost:4433";
 		String ca_cert = "certs/ca_cert.crt";
-		gramine_jni jni_so = new gramine_jni(ip_port.getBytes(), ca_cert.getBytes());
+		gramine_jni jni_so = new gramine_jni(ip_port, ca_cert);
 
 		// demonstrate get key from server
 		int key_len = 64;
@@ -81,7 +83,7 @@ public class clf_test {
 		String fname = "README.md";
 		long[] data_len = new long[1];
 		try {
-			jni_so.GetFileSize(fname.getBytes(), data_len);
+			jni_so.GetFileSize(fname, data_len);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
@@ -95,7 +97,7 @@ public class clf_test {
 		for(int i = 0; i<2000; i++) {
 			System.out.format("\n[test] get data from server. %s, offset=%d, expect len=%d\n", fname, offset, len);
 			try {
-				jni_so.GetFile2Buff(fname.getBytes(), offset, data, len, ret_len);
+				jni_so.GetFile2Buff(fname, offset, data, len, ret_len);
 				print_array(data, ret_len[0]);
 				offset += len;
 				if(offset > (int)data_len[0])
@@ -109,7 +111,7 @@ public class clf_test {
 		System.out.format("\n[test] put result to server\n");
 		String output_fname = "result.out";
 		try {
-			jni_so.PutResult(output_fname.getBytes(), 0, data, 10, ret_len);
+			jni_so.PutResult(output_fname, 0, data, 10, ret_len);
 			System.out.format("jni_so.PutResult(%s)", output_fname);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
