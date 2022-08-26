a1c716c42f57961352ed9a196ad8047dfe1493e7
Liang Fang <liang.a.fang@intel.com>
2022-08-26 16:56:16 +0800

    modify code to fit Gramine code change
    Gramine changed secret_prov file path and API, we do code change to fit that.
    
    Signed-off-by: Liang Fang <liang.a.fang@intel.com>

--- cczoo/cross_lang_framework/clf_client/clf_remote.c
+++ cczoo/cross_lang_framework/clf_client/clf_remote.c
@@ -40,8 +40,7 @@
 
 #define clean_secret_prov()		\
 	do{\
-		secret_provision_destroy();\
-		secret_provision_close(&ctx);\
+		secret_provision_close(ctx);\
 	}while(0)
 
 
@@ -50,7 +49,7 @@
  */
 int get_key(int8_t* ip_port, int8_t* ca_cert, int8_t* key, int32_t key_len) {
 	int ret = STATUS_FAIL;
-	struct ra_tls_ctx ctx = {0};
+	struct ra_tls_ctx* ctx = NULL;
 	uint8_t* secret = NULL;
 	size_t secret_size = 0;
 
@@ -61,7 +60,7 @@ int get_key(int8_t* ip_port, int8_t* ca_cert, int8_t* key, int32_t key_len) {
 	/* connect server */
 	start_secret_prov();
 
-	ret = secret_provision_get(&secret, &secret_size);
+	ret = secret_provision_get(ctx, &secret, &secret_size);
 	if (ret < 0) {
 		log_error("[error] secret_provision_get() returned %d\n", ret);
 		goto out;
@@ -96,7 +95,7 @@ out:
 int remote_get_file_size(int8_t* ip_port, int8_t* ca_cert, char* fname, int64_t* ret_len) {
 	int ret = STATUS_FAIL;
 	int bytes;
-	struct ra_tls_ctx ctx = {0};
+	struct ra_tls_ctx* ctx = NULL;
 	msg_req_t req = {0};
 	msg_resp_t resp = {0};
 
@@ -111,14 +110,14 @@ int remote_get_file_size(int8_t* ip_port, int8_t* ca_cert, char* fname, int64_t*
 	req.msg_type = MSG_GET_DATA_SIZE;
 	req.data_len = 0;
 	strncpy((char*)req.get_size.fname, fname, MAX_FNAME_LEN-1);
-	bytes = secret_provision_write(&ctx, (uint8_t*)&req, sizeof(msg_req_t));
+	bytes = secret_provision_write(ctx, (uint8_t*)&req, sizeof(msg_req_t));
 	if (bytes < 0) {
 		log_error("[error] secret_provision_write() returned %d\n", bytes);
 		goto out;
 	}
 
 	/* get size from source  */
-	bytes = secret_provision_read(&ctx, (uint8_t*)&resp, sizeof(msg_resp_t));
+	bytes = secret_provision_read(ctx, (uint8_t*)&resp, sizeof(msg_resp_t));
 	if (bytes != sizeof(msg_resp_t) || STATUS_SUCCESS != resp.status) {
 		log_error("[error] secret_provision_read() returned %d (expected %lu) resp.status=%X\n",
 			bytes, sizeof(msg_resp_t), resp.status);
@@ -140,7 +139,7 @@ out:
 int remote_get_file_2_buff(int8_t* ip_port, int8_t* ca_cert, char* fname, int64_t offset, int8_t* data, int32_t len, int32_t* ret_len) {
 	int ret = STATUS_FAIL;
 	int bytes;
-	struct ra_tls_ctx ctx = {0};
+	struct ra_tls_ctx* ctx = NULL;
 	msg_req_t req = {0};
 	msg_resp_t resp = {0};
 
@@ -157,14 +156,14 @@ int remote_get_file_2_buff(int8_t* ip_port, int8_t* ca_cert, char* fname, int64_
 	req.get_data.offset = offset;
 	req.get_data.len = len;
 	strncpy((char*)req.get_data.fname, fname, MAX_FNAME_LEN-1);
-	bytes = secret_provision_write(&ctx, (uint8_t*)&req, sizeof(msg_req_t));
+	bytes = secret_provision_write(ctx, (uint8_t*)&req, sizeof(msg_req_t));
 	if (bytes < 0) {
 		log_error("[error] secret_provision_write() returned %d\n", bytes);
 		goto out;
 	}
 
 	/* get data from source  */
-	bytes = secret_provision_read(&ctx, (uint8_t*)&resp, sizeof(msg_resp_t));
+	bytes = secret_provision_read(ctx, (uint8_t*)&resp, sizeof(msg_resp_t));
 	if (bytes != sizeof(msg_resp_t) || STATUS_SUCCESS != resp.status) {
 		log_error("[error] secret_provision_read() returned %d (expected %lu) resp.status=%X\n",
 			bytes, sizeof(msg_resp_t), resp.status);
@@ -172,7 +171,7 @@ int remote_get_file_2_buff(int8_t* ip_port, int8_t* ca_cert, char* fname, int64_
 	}
 
 	uint64_t data_len = len < resp.get_data.data_len ? len : resp.get_data.data_len;
-	bytes = secret_provision_read(&ctx, (uint8_t*)data, data_len);
+	bytes = secret_provision_read(ctx, (uint8_t*)data, data_len);
 	if (bytes != data_len) {
 		log_error("[error] secret_provision_read() returned %d (expected %lu)\n",
 			bytes, data_len);
@@ -195,7 +194,7 @@ out:
 int remote_put_result(int8_t* ip_port, int8_t* ca_cert, char* fname, int64_t offset, int8_t* data, int32_t len, int32_t* ret_len) {
 	int ret = STATUS_FAIL;
 	int bytes;
-	struct ra_tls_ctx ctx = {0};
+	struct ra_tls_ctx* ctx = NULL;
 	msg_req_t req = {0};
 	msg_resp_t resp = {0};
 
@@ -212,20 +211,20 @@ int remote_put_result(int8_t* ip_port, int8_t* ca_cert, char* fname, int64_t off
 	req.put_res.offset = offset;
 	req.put_res.len = len;
 	strncpy((char*)req.put_res.fname, fname, MAX_FNAME_LEN-1);
-	bytes = secret_provision_write(&ctx, (uint8_t*)&req, sizeof(msg_req_t));
+	bytes = secret_provision_write(ctx, (uint8_t*)&req, sizeof(msg_req_t));
 	if (bytes < 0) {
 		log_error("[error] secret_provision_write() returned %d\n", bytes);
 		goto out;
 	}
 
-	bytes = secret_provision_write(&ctx, (uint8_t*)data, len);
+	bytes = secret_provision_write(ctx, (uint8_t*)data, len);
 	if (bytes != len) {
 		log_error("[error] secret_provision_write() returned %d, expect %d\n", bytes, len);
 		goto out;
 	}
 
 	/* get response */
-	bytes = secret_provision_read(&ctx, (uint8_t*)&resp, sizeof(msg_resp_t));
+	bytes = secret_provision_read(ctx, (uint8_t*)&resp, sizeof(msg_resp_t));
 	if (bytes != sizeof(msg_resp_t) || STATUS_SUCCESS != resp.status) {
 		log_error("[error] secret_provision_read() returned %d (expected %lu) resp.status=%X\n",
 			bytes, sizeof(msg_resp_t), resp.status);
--- cczoo/cross_lang_framework/clf_client/debug.c
+++ cczoo/cross_lang_framework/clf_client/debug.c
@@ -34,7 +34,7 @@
 int test_secret_prov_connect() {
     int ret;
 
-    struct ra_tls_ctx ctx = {0};
+    struct ra_tls_ctx* ctx = NULL;
 
     bool is_constructor = false;
     char* str = getenv(SECRET_PROVISION_CONSTRUCTOR);
@@ -53,8 +53,7 @@ int test_secret_prov_connect() {
 
     ret = 0;
 out:
-    secret_provision_destroy();
-    secret_provision_close(&ctx);
+    secret_provision_close(ctx);
     return ret;
 }
 
@@ -77,73 +76,6 @@ static int list_dir(char *path) {
 }
 
 int secret_prov_test() {
-    int ret;
-    int bytes;
-
-    struct ra_tls_ctx ctx = {0};
-
-    uint8_t* secret1   = NULL;
-    size_t secret1_size = 0;
-
-    uint8_t secret2[3] = {0}; /* we expect second secret to be 2-char string */
-
-    bool is_constructor = false;
-    char* str = getenv(SECRET_PROVISION_CONSTRUCTOR);
-    if (str && (!strcmp(str, "1") || !strcmp(str, "true") || !strcmp(str, "TRUE")))
-        is_constructor = true;
-
-    list_dir(".");
-    list_dir("./certs");
-
-    if (!is_constructor) {
-        /* secret provisioning was not run as part of initialization, run it now */
-        ret = secret_provision_start("VM-0-12-ubuntu:4433",
-                                     "certs/ca_cert.crt", &ctx);
-        if (ret < 0) {
-            log_error("[error] secret_provision_start() returned %d\n", ret);
-            goto out;
-        }
-    }
-
-    ret = secret_provision_get(&secret1, &secret1_size);
-    if (ret < 0) {
-        log_error("[error] secret_provision_get() returned %d\n", ret);
-        goto out;
-    }
-    if (!secret1_size) {
-        log_error("[error] secret_provision_get() returned secret with size 0\n");
-        goto out;
-    }
-
-    secret1[secret1_size - 1] = '\0';
-
-    if (!is_constructor) {
-        /* let's ask for another secret (just to show communication with secret-prov server) */
-        bytes = secret_provision_write(&ctx, (uint8_t*)SEND_STRING, sizeof(SEND_STRING));
-        if (bytes < 0) {
-            log_error("[error] secret_provision_write() returned %d\n", bytes);
-            goto out;
-        }
-
-        /* the secret we expect in return is a 2-char string */
-        bytes = secret_provision_read(&ctx, secret2, sizeof(secret2));
-        if (bytes < 0) {
-            log_error("[error] secret_provision_read() returned %d\n", bytes);
-            goto out;
-        }
-        if (bytes != sizeof(secret2)) {
-            log_error("[error] secret_provision_read() returned secret with size %d"
-                    " (expected %lu)\n", bytes, sizeof(secret2));
-            goto out;
-        }
-
-        secret2[bytes - 1] = '\0';
-    }
-
-    printf("--- Received secret1 = '%s', secret2 = '%s' ---\n", secret1, secret2);
-    ret = 0;
-out:
-    secret_provision_destroy();
-    secret_provision_close(&ctx);
-    return ret;
+	return 0;
 }
+
