1131d61a5656e39939052086ab2a94be0aced363
Liang Fang <liang.a.fang@intel.com>
2022-10-20 17:37:57 +0800

    Fix bugs in parameter parsing
    Signed-off-by: Liang Fang <liang.a.fang@intel.com>

--- cczoo/cross_lang_framework/clf_server/cmd_params.h
+++ cczoo/cross_lang_framework/clf_server/cmd_params.h
@@ -22,20 +22,21 @@
 #define WRAP_KEY_SIZE	16
 #define MRSIGNER_LEN	32
 #define MRENCLAVE_LEN	32
-#endif
+
 struct cmd_params {
-    const char MRSigner[MRSIGNER_LEN];
-    const char MREnclave[MRENCLAVE_LEN];
+    char MRSigner[MRSIGNER_LEN];
+    char MREnclave[MRENCLAVE_LEN];
     uint16_t isv_prod_id;
     uint16_t isv_svn;
-    const char secret[WRAP_KEY_SIZE];
-    const char port[8];
-    const char server_cert_path[PATH_MAX];
-    const char server_private_key_path[PATH_MAX];
+    char secret[WRAP_KEY_SIZE * 2 + 1];
+    uint16_t port;
+    char server_cert_path[PATH_MAX];
+    char server_private_key_path[PATH_MAX];
 };
 
 /**
- * Function for parsing command-line parameters of quote_gen
- *
+ * Function for parsing command-line parameters
  */
 int cmd_params_process(int argc, char **argv, struct cmd_params *params);
+
+#endif
