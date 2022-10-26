ac1de236537f28db98dddb23439773cd495c8734
ubuntu <ubuntu@localhost.localdomain>
2022-10-26 17:25:37 +0800

    revise convert_docker file and add readme for ccp

--- cczoo/cross_lang_framework/ccp/convert_clf_server_to_ccp_image.sh
+++ cczoo/cross_lang_framework/ccp/convert_clf_server_to_ccp_image.sh
@@ -15,18 +15,25 @@
 
 #!/bin/bash
 set -e
-
+COLORLESS="\033[0m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
 #below is fake secret, you need to replace with your valid ones
-ccp-cli pack --app-entry="/clf/cczoo/cross_lang_framework/clf_server/clf_server" \
-             --memsize=8192M --thread=64 \
-             --tmpl=default \
-             --secret-id=AKID3vXswkIAl59vQebIjskMhlY8KJ1RbU6S \
-             --secret-key=6vTklUw83i6C1Zrra3GBpCI8gy9NTIr9 \
-             --capp-id=capp-Y2IyNGM1YzAt \
-             --app-image=clf-server:gramine1.3-ubuntu20.04 \
-             --app-type=image \
-             --start=/clf/cczoo/cross_lang_framework/clf_server
-
+echo -e "ccp-cli pack ${GREEN}--app-entry${COLORLESS}=\"/clf/cczoo/cross_lang_framework/clf_server/clf_server\"\\"             
+echo -e "             ${GREEN}--memsize${COLORLESS}=8192M ${GREEN}--thread${COLORLESS}=64\\"             
+echo -e "             ${GREEN}--tmpl${COLORLESS}=default\\"             
+echo -e "             ${GREEN}--secret-id${COLORLESS}=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\\"             
+echo -e "             ${GREEN}--secret-key${COLORLESS}=kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk\\"             
+echo -e "             ${GREEN}--capp-id${COLORLESS}=capp-Y2IyNGM1YzAt\\"             
+echo -e "             ${GREEN}--app-image${COLORLESS}=clf-server:gramine1.3-ubuntu20.04\\"             
+echo -e "             ${GREEN}--app-type${COLORLESS}=image\\"             
+echo -e "             ${GREEN}--start${COLORLESS}=/clf/cczoo/cross_lang_framework/clf_server"
+echo -e ""
 # just an example about how to run
-docker run -it -p 4433:4433 --device /dev/sgx_enclave --device /dev/sgx_provision -v /home/ubuntu/liang/confidential-computing-zoo/cczoo/cross_lang_framework/clf_server/certs:/clf/cczoo/cross_lang_framework/clf_server/certs -v /home/ubuntu/liang/confidential-computing-zoo/cczoo/cross_lang_framework/clf_server/clf_server.conf:/clf/cczoo/cross_lang_framework/clf_server/clf_server.conf --add-host=VM-0-3-ubuntu:10.206.0.3 sec_clf-server:gramine1.3-ubuntu20.04
+echo -e "docker run -it ${GREEN}-p${COLORLESS} 4433:4433 ${GREEN}--device${COLORLESS} /dev/sgx_enclave ${GREEN}--device${COLORLESS} /dev/sgx_provision\\"
+echo -e "             ${GREEN}--v${COLORLESS} /home/confidential-computing-zoo/cczoo/cross_lang_framework/clf_server/certs:/clf/cczoo/cross_lang_framework/clf_server/certs\\"
+echo -e "             ${GREEN}--v${COLORLESS} /home/confidential-computing-zoo/cczoo/cross_lang_framework/clf_server/clf_server.conf:/clf/cczoo/cross_lang_framework/clf_server/clf_server.conf\\"             
+echo -e "             ${GREEN}--add-host${COLORLESS}=VM-0-3-ubuntu:10.206.0.3 clf-server:gramine1.3-ubuntu20.04"
+echo -e ""
 
