1dc27c5d0cfc11b89d2e4ed5cf10312abf929279
Liang Fang <liang.a.fang@intel.com>
2022-11-02 15:06:45 +0800

    add commandline option to gen_cert.sh
    Signed-off-by: Liang Fang <liang.a.fang@intel.com>

--- cczoo/cross_lang_framework/tools/gen_cert.sh
+++ cczoo/cross_lang_framework/tools/gen_cert.sh
@@ -15,13 +15,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CA_KEY=ca_private_key.key
+CA_KEY=ca_private_key.pem
 CA_CERT=ca_cert.crt
-CHILD_KEY=child_private_key.key
-CHILD_CERT=child_cert.crt
+CHILD_KEY=server_private_key.pem
+CHILD_CERT=server_signed_cert.crt
+
+REQ_FILE=__my_cert_req.csr
+EXT_FILE=__v3.ext
+SRL_FILE=`echo ${CA_CERT} | awk -F '.' '{print $1}'`.srl
+
+# color
+COLORLESS="\033[0m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
 
 gen_root_cert() {
-    echo -e "========================================="
+    echo -e "\n========================================="
     echo -e "Generate Self-signed Root Certification:"
     echo -e "========================================="
     openssl req -newkey rsa:2048 \
@@ -34,35 +44,38 @@ gen_root_cert() {
 }
 
 gen_sign_req() {
-    echo -e "Generate sign req:"
+    echo -e "\n[Generate sign req]: ..."
     openssl req -new -nodes \
                -newkey rsa:2048 \
                -keyout ${CHILD_KEY} \
-               -out my_cert_req.csr
+               -out ${REQ_FILE}
+    echo -e "${GREEN}done${COLORLESS}"
 }
 
 create_v3_ext() {
-    echo -e "Creating v3.ext:"
+    echo -e "\n[Creating ${EXT_FILE}]: ... \c"
     echo -e "\
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
-keyUsage=digitalSignature,nonRepudiation,keyEncipherment,dataEncipherment" >v3.ext
+keyUsage=digitalSignature,nonRepudiation,keyEncipherment,dataEncipherment" >${EXT_FILE}
+    echo -e "${GREEN}... done${COLORLESS}"
 }
 
 sign_child_cert() {
-    echo -e "Sign child certification:"
+    echo -e "\n[Sign child certification]: ..."
     openssl x509 -req \
-               -in my_cert_req.csr \
+               -in ${REQ_FILE} \
                -days 365 \
-               -extfile v3.ext \
+               -extfile ${EXT_FILE} \
                -CA ${CA_CERT}  \
                -CAkey ${CA_KEY}  \
                -CAcreateserial \
                -out ${CHILD_CERT}
+    echo -e "${GREEN}... done${COLORLESS}"
 }
 
 display_result() {
-    echo -e "* root private key:"
+    echo -e "\n* root private key:"
     echo -e "$(find `pwd` -name ${CA_KEY})"
     echo -e "* root certification:"
     echo -e "$(find `pwd` -name ${CA_CERT})"
@@ -73,8 +86,14 @@ display_result() {
     echo -e "$(find `pwd` -name ${CHILD_CERT})"
 }
 
+do_clean() {
+    rm ${REQ_FILE}
+    rm ${EXT_FILE}
+    rm ${SRL_FILE}
+}
+
 generate_child_cert() {
-    echo -e "========================================="
+    echo -e "\n========================================="
     echo -e "Generate Child Certification"
     echo -e "========================================="
 
@@ -85,7 +104,36 @@ generate_child_cert() {
     sign_child_cert
 
     display_result
+
+    do_clean
 }
 
-gen_root_cert
-generate_child_cert
+helper() {
+    echo -e "./gen_cert.sh [-r] [-c]"
+    echo -e "-r\tGenerate root certification (e.g. ca_cert.crt), used in clf_client." | sed "s/./&\n\t/72;P;D"
+    echo -e "-c\tGenerate child certification and private key, used in clf_server." | sed "s/./&\n\t/72;P;D"
+    echo -e "\tRoot certification is used to sign the child certification, so root certification should be generated first.\
+ The generated files should be put into folder clf_server/certs/, replace existing files:\
+ (e.g. server_private_key.pem and server_signed_cert.crt)" | sed "s/./&\n\t/72;P;D"
+}
+
+if [ $# -eq 0 ];
+then
+    helper
+fi
+
+while getopts "hrc" OPT &> /dev/null ; do
+    case "$OPT" in
+        h)
+            helper
+            exit 0 ;;
+        r)
+            gen_root_cert ;;
+        c)
+            generate_child_cert ;;
+        *)
+            echo -e "Invalid Parameters."
+            helper
+            exit 1 ;;
+    esac
+done
