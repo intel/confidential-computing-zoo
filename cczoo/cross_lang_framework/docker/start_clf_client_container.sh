b1d9dc1e7d7d539feabb3c4b36e27e212fe35861
pengyuabc <pengyux.ji@intel.com>
2023-05-28 19:30:17 +0800

    Updated the script to start the container (#224)
    * update clf
    
    * update clf

--- cczoo/cross_lang_framework/docker/start_clf_client_container.sh
+++ cczoo/cross_lang_framework/docker/start_clf_client_container.sh
@@ -22,16 +22,25 @@ else
     ip_addr=127.0.0.1
 fi
 
+# Please fill in the domain name where clf_server is deployed
 if  [ -n "$2" ] ; then
-    image_tag=$2
+    clf_server_domain_name=$2
 else
-    image_tag=clf-client:gramine1.3-ubuntu20.04
+    clf_server_domain_name=`hostname`
 fi
+
+if  [ -n "$3" ] ; then
+    image_tag=$3
+else
+    image_tag=clf-client-app:gramine1.3-ubuntu20.04
+fi
+
 
 # You can remove no_proxy and proxy_server if your network doesn't need it
 no_proxy="localhost,127.0.0.1"
 proxy_server="" # your http proxy server
 
+# You need to get ca_cert.crt by running the file ../tools/gen_cert.sh before running this script
 docker run -it \
     --restart=unless-stopped \
     --cap-add=SYS_PTRACE \
@@ -39,9 +48,13 @@ docker run -it \
     --device=/dev/sgx_enclave:/dev/sgx/enclave \
     --device=/dev/sgx_provision:/dev/sgx/provision \
     --add-host=pccs.service.com:${ip_addr} \
+    --add-host=${clf_server_domain_name}:${ip_addr} \
     -e no_proxy=${no_proxy} \
     -e http_proxy=${proxy_server} \
     -e https_proxy=${proxy_server} \
     -v /home:/home/host-home \
+    -v `pwd -P`/../tools/ca_cert.crt:/clf/cczoo/cross_lang_framework/clf_client/app/certs/ca_cert.crt \
+    -v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
     ${image_tag} \
     bash
+
