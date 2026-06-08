d65fdead9dd0bb09dc66dd3598ac357ef2306db2
Hsy-Intel <siyuan.hui@intel.com>
2026-06-08 09:53:34 +0800

    Add license for tc-api, tlog and OpenClaw

--- cczoo/agent-cc/core/tc-api/scripts/common.sh
+++ cczoo/agent-cc/core/tc-api/scripts/common.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright (c) 2026 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 TC_API_COMMON_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TC_API_REPO_ROOT="$(cd "$TC_API_COMMON_DIR/.." && pwd)"
 TC_API_VENV_DIR="$TC_API_REPO_ROOT/venv"
